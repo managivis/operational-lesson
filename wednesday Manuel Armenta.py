80ffbfad2d8f779567259ad97475e51c06dac1c8
managivis <marmenta44@pimajted.net>
2023-02-01 17:25:52 -0700

    Create wednesday Manuel Armenta.py

---
+++ wednesday Manuel Armenta.py
@@ -0,0 +1,22 @@
+import random
+correctAnswer = random.randint(1, 100)
+gameOver = False
+
+while gameOver == False:
+
+    playerGuess = int(input("Guess a number between 1 and 100: "))
+
+    if playerGuess == correctAnswer:
+        compareAnswer = "Right"
+        gameOver = True
+    elif playerGuess > correctAnswer:
+        compareAnswer = "High"
+    elif playerGuess < correctAnswer:
+        compareAnswer = "Low"
+
+    if compareAnswer == "Right":
+        print("Correct! You Win!")
+    elif compareAnswer == "high":
+        print("Too High! Guess Again!")
+    elif compareAnswer == "Low":
+        print("Too Low! Guess Again!")
